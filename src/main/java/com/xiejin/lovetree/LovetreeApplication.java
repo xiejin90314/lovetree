d99f7680ec64080487a6ef4fa35a543df3b8be4e
xiejin <xiejin@qianbaocard.com>
2018-04-18 13:46:39 +0800

    提交代码

---
+++ src/main/java/com/xiejin/lovetree/LovetreeApplication.java
@@ -0,0 +1,12 @@
+package com.xiejin.lovetree;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LovetreeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LovetreeApplication.class, args);
+	}
+}
